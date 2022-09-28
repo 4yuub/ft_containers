300eaa48b78adfd64e0c17e364493798946c2ae4
4yuub <karafi.work@gmail.com>
2022-09-28 02:11:18 +0100

    add a test for reverse iterator

--- common/iterator_test.cpp
+++ common/iterator_test.cpp
@@ -14,10 +14,14 @@ class MyIterator : public NS::iterator<std::input_iterator_tag, int>
 {
   int* p;
 public:
+  typedef ptrdiff_t differece_t;
+  MyIterator() {}
   MyIterator(int* x) :p(x) {}
   MyIterator(const MyIterator& mit) : p(mit.p) {}
   MyIterator& operator++() {++p;return *this;}
+  MyIterator& operator--() {--p;return *this;}
   MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
+  MyIterator operator--(int) {MyIterator tmp(*this); operator--(); return tmp;}
   bool operator==(const MyIterator& rhs) const {return p==rhs.p;}
   bool operator!=(const MyIterator& rhs) const {return p!=rhs.p;}
   int& operator*() {return *p;}
@@ -25,6 +29,7 @@ public:
 
 int test_iterator(void)
 {
+  std::cout << "iterator test: \n";
   int numbers[]={10,20,30,40,50};
   MyIterator from(numbers);
   MyIterator until(numbers+5);
@@ -32,6 +37,12 @@ int test_iterator(void)
     std::cout << *it << ' ';
   std::cout << '\n';
 
+  std::cout << "reverse iterator test: \n";
+  NS::reverse_iterator<MyIterator> rfrom(until);
+  NS::reverse_iterator<MyIterator> runtil(from);
+  for (NS::reverse_iterator<MyIterator> it=rfrom; it.base()!=runtil.base(); it++)
+    std::cout << *it << ' ';
+  std::cout << '\n';
   return 0;
 }
 
