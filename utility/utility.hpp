0008a279a24b4cc18b0638968d9e8a6e65b31869
4yuub <karafi.work@gmail.com>
2022-10-28 01:55:25 +0100

    add relational operator for pair

--- utility/utility.hpp
+++ utility/utility.hpp
@@ -34,6 +34,38 @@ namespace ft {
     {
         return pair<T1, T2>(x, y);
     }
+
+    // relational operators
+    template <class T1, class T2>
+    bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+        return lhs.first == rhs.first && lhs.second == rhs.second;
+    }
+
+    template <class T1, class T2>
+    bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+        return !(lhs == rhs);
+    }
+
+    template <class T1, class T2>
+    bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+        return lhs.first < rhs.first \
+            || (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+    }
+
+    template <class T1, class T2>
+    bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+        return !(rhs < lhs);
+    }
+
+    template <class T1, class T2>
+    bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+        return rhs < lhs;
+    }
+
+    template <class T1, class T2>
+    bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+        return !(lhs < rhs);
+    }
 } // namespace ft
 
 #endif
